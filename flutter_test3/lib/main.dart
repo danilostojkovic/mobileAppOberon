b3f1cc927bd92f89d37504d413e5805941d26983
Matteo Casonato <matteocasonato@protonmail.com>
2022-02-14 20:37:11 +0100

    qr code after wallet connection

--- flutter_test3/lib/main.dart
+++ flutter_test3/lib/main.dart
@@ -1,15 +1,21 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:dart_web3/dart_web3.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 //String _url = "";
 const String privateKey =
     'ae17644f94057fe74c4dd000b0f3594779bb05fc69b7edaabc34be2a77b08456';
 const String rpcUrl = 'https://api.avax-test.network/ext/bc/C/rpc';
 var addressHex = "";
-var balance, session;
+var balance, session, account;
+Barcode? result;
 
 void main() async {
   final client = Web3Client(rpcUrl, Client());
@@ -52,136 +58,316 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<String> _value = Future.value("");
+  _walletConnect() async {
+    final connector = WalletConnect(
+      bridge: 'https://bridge.walletconnect.org',
+      clientMeta: const PeerMeta(
+        name: 'WalletConnect',
+        description: 'WalletConnect Developer App',
+        url: 'https://walletconnect.org',
+        icons: [
+          'https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
+        ],
+      ),
+    );
 
-  @override
-  initState() {
-    super.initState();
-    _value = _walletConnect();
+    // Subscribe to events
+    connector.on('connect', (session) => print(session));
+    connector.on('session_update', (payload) => print(payload));
+    connector.on('disconnect', (session) => print(session));
+
+    // Create a new session
+    if (!connector.connected) {
+      session = await connector.createSession(
+          chainId: 43113,
+          onDisplayUri: (uri) async => {print(uri), await launch(uri)});
+    }
+
+    setState(() {
+      account = session.accounts[0];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<String>(
-      future: _value,
-      initialData: 'App Name',
-      builder: (
-        BuildContext context,
-        AsyncSnapshot<String> snapshot,
-      ) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+    if (account == null) {
+      return _buttonConnectWallet();
+    } else {
+      return _buttonScanQRCode();
+    }
+  }
+
+  _buttonConnectWallet() {
+    return Scaffold(
+        backgroundColor: Colors.blueAccent.shade200,
+        body: Center(
+          child: Column(
             children: [
-              CircularProgressIndicator(),
-              Visibility(
-                visible: snapshot.hasData,
-                child: Text(
-                  "${snapshot.data}",
-                  style: const TextStyle(color: Colors.black, fontSize: 24),
+              Image.asset("assets/LogoShopChainTest5.png"),
+              const Text("Account:", style: TextStyle(fontSize: 20)),
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child:
+                      Text("$account", style: const TextStyle(fontSize: 30))),
+              const SizedBox(height: 30),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF0D47A1),
+                              Color(0xFF1976D2),
+                              Color(0xFF42A5F5),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () async => _walletConnect(),
+                      child: const Text('Connect Wallet'),
+                    ),
+                  ],
                 ),
-              )
+              ),
             ],
-          );
-        } else if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return const Text('Error');
-          } else if (snapshot.hasData) {
-            return Text("${snapshot.data}",
-                style: const TextStyle(color: Colors.teal, fontSize: 36));
-          } else {
-            return const Text('Empty data');
-          }
-        } else {
-          return Text('State: ${snapshot.connectionState}');
-        }
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ));
+  }
+
+  _buttonScanQRCode() {
+    return Scaffold(
+        backgroundColor: Colors.blueAccent.shade200,
+        body: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/LogoShopChainTest5.png"),
+              if (result != null) Text('Result: ${result!.code}'),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Stack(
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF0D47A1),
+                              Color(0xFF1976D2),
+                              Color(0xFF42A5F5),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16.0),
+                        primary: Colors.white,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).push(_createRoute1());
+                      },
+                      child: const Text('Scan QRCode'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ));
+  }
+
+  Route _createRoute1() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          const QRScanPage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
       },
     );
   }
 }
 
-Scaffold _buildScaffold(var uri) {
-  return Scaffold(
-      backgroundColor: Colors.blueAccent.shade200,
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset("assets/LogoShopChainTest5.png"),
-            Text("Address: ", style: TextStyle(fontSize: 20)),
-            Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Text("$addressHex", style: TextStyle(fontSize: 30))),
-            Text("Balance:", style: TextStyle(fontSize: 20)),
-            Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                child: Text("$balance", style: TextStyle(fontSize: 30))),
-            const SizedBox(height: 30),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(4),
-              child: Stack(
+class QRScanPage extends StatefulWidget {
+  const QRScanPage({Key? key}) : super(key: key);
+
+  @override
+  _QRScanPageState createState() => _QRScanPageState();
+}
+
+class _QRScanPageState extends State<QRScanPage> {
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(
+            flex: 1,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Positioned.fill(
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: <Color>[
-                            Color(0xFF0D47A1),
-                            Color(0xFF1976D2),
-                            Color(0xFF42A5F5),
-                          ],
+                  if (result != null)
+                    Text('Data: ${result!.code}')
+                  else
+                    const Text('Scan a code'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.toggleFlash();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getFlashStatus(),
+                              builder: (context, snapshot) {
+                                return Text('Flash: ${snapshot.data}');
+                              },
+                            )),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.flipCamera();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getCameraInfo(),
+                              builder: (context, snapshot) {
+                                if (snapshot.data != null) {
+                                  return Text(
+                                      'Camera facing ${describeEnum(snapshot.data!)}');
+                                } else {
+                                  return const Text('loading');
+                                }
+                              },
+                            )),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.pauseCamera();
+                          },
+                          child: const Text('pause',
+                              style: TextStyle(fontSize: 20)),
                         ),
                       ),
-                    ),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      padding: const EdgeInsets.all(16.0),
-                      primary: Colors.white,
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () => _launchURL(uri),
-                    child: const Text('Gradient'),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.resumeCamera();
+                          },
+                          child: const Text('resume',
+                              style: TextStyle(fontSize: 20)),
+                        ),
+                      )
+                    ],
                   ),
                 ],
               ),
             ),
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-      ));
-}
-
-Future<String> _walletConnect() async {
-  var _uri;
-
-  final connector = WalletConnect(
-    bridge: 'https://bridge.walletconnect.org',
-    clientMeta: const PeerMeta(
-      name: 'WalletConnect',
-      description: 'WalletConnect Developer App',
-      url: 'https://walletconnect.org',
-      icons: [
-        'https://gblobscdn.gitbook.com/spaces%2F-LJJeCjcLrr53DcT1Ml7%2Favatar.png?alt=media'
-      ],
-    ),
-  );
-
-  // Subscribe to events
-  connector.on('connect', (session) => print(session));
-  connector.on('session_update', (payload) => print(payload));
-  connector.on('disconnect', (session) => print(session));
-
-  // Create a new session
-  if (!connector.connected) {
-    session = await connector.createSession(
-        chainId: 43113, onDisplayUri: (uri) => {_uri = uri});
-    return Future.value(session.toUri());
+          )
+        ],
+      ),
+    );
   }
 
-  return Future.error("PORCA VACCA");
-}
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 300.0
+        : 400.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
 
-void _launchURL(var url) async {
-  if (!await launch(url)) throw 'Could not launch $url';
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 }
